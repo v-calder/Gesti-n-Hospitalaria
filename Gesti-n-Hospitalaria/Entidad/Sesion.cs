eb837ac8556f7769e41f808663678b071bbe3af0
Victor Calderon <vcalderons@miumg.edu.gt>
2026-05-20 21:33:31 -0600

    Creacion del form Login con sus respectivas clases, para la capa datos, entidad y megocio

---
+++ Gesti-n-Hospitalaria/Entidad/Sesion.cs
@@ -0,0 +1,11 @@
+﻿namespace Entidad
+{
+    public static class Sesion
+    {
+        public static int CodigoUsuario { get; set; }
+        public static string NombreUsuario { get; set; }
+        public static string NombreRol { get; set; }
+        public static int CodigoRol { get; set; }
+    }
+}
+
